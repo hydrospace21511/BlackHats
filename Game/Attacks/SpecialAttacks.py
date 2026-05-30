a63f3e1b55bb58ca47c9da65f191f66f8e8afab4
hydrospace21511 <hydrospace21511@gmail.com>
2026-05-29 21:12:11 -0300

    DarkHats v1.6 Items and System optimization

--- Game/Attacks/SpecialAttacks.py
+++ Game/Attacks/SpecialAttacks.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pygame
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 from time import sleep
 from Game.Classes.SecretClasses.Hatsune import HatsuneMikuClass
 from Game.Classes.SecretClasses.CorruptedHatsune import CorruptedHatsuneMikuClass
