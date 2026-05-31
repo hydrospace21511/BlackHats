95ba29550d8fd6ff1481c851a978f6bc8c0de8c7
hydrospace21511 <hydrospace21511@gmail.com>
2026-05-31 18:45:54 -0300

    DarkHats v1.7.3 Linux Compatibility

--- Game/Main/COBALT.py
+++ Game/Main/COBALT.py
@@ -1,7 +1,7 @@
-import keyboard
 from time import sleep
 from colorama import Fore, Style, init
 import os
+import sys
 from Game.Main.Color import cText
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -50,13 +50,37 @@ class COBALT:
                 break
         sleep(2)  
 
+    def _wait_for_space(self):
+        if os.name == 'nt':
+            import msvcrt
+            print(f"{Fore.GREEN}Press SPACE to hack...")
+            while True:
+                if msvcrt.kbhit() and msvcrt.getch() == b' ':
+                    break
+        else:
+            try:
+                import tty
+                import termios
+            except ImportError:
+                input(f"{Fore.GREEN}Press SPACE then ENTER to hack...")
+                return
+
+            print(f"{Fore.GREEN}Press SPACE to hack...")
+            fd = sys.stdin.fileno()
+            old_settings = termios.tcgetattr(fd)
+            try:
+                tty.setcbreak(fd)
+                while True:
+                    ch = sys.stdin.read(1)
+                    if ch == ' ':
+                        break
+            finally:
+                termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
     def _Start(self):
-        
         self._print_menu()
-        while True:
-            if keyboard.is_pressed('space'):
-                self._Load_Menu()
-                break
+        self._wait_for_space()
+        self._Load_Menu()
         sleep(0.05)
             
                  
